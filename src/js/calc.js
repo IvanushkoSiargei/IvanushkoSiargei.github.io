26e1816d8ec0a5f9a0f75d9168ed96bb5c3fb1ca
IvanushkoSiargei <114254539+IvanushkoSiargei@users.noreply.github.com>
2022-10-31 17:41:26 +0300

    Add files via upload

--- src/js/calc.js
+++ src/js/calc.js
@@ -1,121 +1,54 @@
+"use strict";
 let d, e, g, h, j, k;
-let calc={
-	saitTen: [
-    	[0],
-    	[2, 1],
-    	[4, 1.5],
-    	[7, 2]
-	],
-	saitDiz: [
-		[0],
-    	[3, 0.5],
-    	[5, 1],
-    	[8, 1.5]
-	],
-	saitAdapt: [
-		[0],
-		[2, 1],
-		[4, 2],
-		[7, 3]
-	],
-	run(){
-		let cena = parseInt(d) + parseInt(g) + parseInt(j);
-		console.log(cena);
-		let time = parseInt(e) + parseInt(h) + parseInt(k);
-		
-	}
-}	
-
-	function saitId(){
-        let a=document.getElementById('sait').value;
-        /*console.log(a);*/ 
-        let d = calc.saitTen[a][0];
-        let e = calc.saitTen[a][1];
-        /*console.log(d);
-        console.log(e);*/
-		return;
-	}
-	function dizId(){
-        let b =document.getElementById('dizSait').value;
-        console.log(b); 
-        let g=calc.saitDiz[b][0];
-        let h=calc.saitDiz[b][1];
-        /*console.log(g);
-        console.log(h);*/
-		return;
-	}
-	function adaptId(){
-        let c=document.getElementById('adapt').value;
-        console.log(c); 
-        let j=calc.saitAdapt[c][0];
-        let k=calc.saitAdapt[c][1];
-        /*console.log(j);
-        console.log(k);*/
-		return;
-	}
-function f(){
-	calc.run();
-	console.log(calc.run.cena);
-	console.log(calc.run.time);
+var  calc = {
+    saitTen : [
+        [0],
+        [10, 1],
+        [15, 1.5],
+        [20, 2]
+    ],
+    saitDiz : [
+        [0],
+        [5, 0.5],
+        [10, 1],
+        [15, 1.5]
+    ],
+    saitAdapt : [
+        [0],
+        [3, 1],
+        [6, 2],
+        [9, 3]
+    ]
 }
-saitId();
-dizId();
-adaptId();
-f();
-
-/*let d, e, g, h, j, k;
-let cena = d+g+j;
-let time = e+h+k;
-
 function saitId(){
         let a=document.getElementById('sait').value;
-        /*console.log(a); 
-        let d=saitTen[a][0];
-        let e=saitTen[a][1];
-        console.log(d);
-        console.log(e);
+        d=calc.saitTen[a][0];
+        e=calc.saitTen[a][1];
+        if (a == 0) {
+            alert("выбери ТИП САЙТА");
+        };
+    return;
 }
 function dizId(){
         let b=document.getElementById('dizSait').value;
-        /*console.log(b); 
-        let g=saitDiz[b][0];
-        let h=saitDiz[b][1];
-        console.log(g);
-        console.log(h);
+        g=calc.saitDiz[b][0];
+        h=calc.saitDiz[b][1];
+        if (b == 0 ) {
+            alert("выбери ДИЗАИН");
+        };
+    return;
 }
 function adaptId(){
         let c=document.getElementById('adapt').value;
-        /*console.log(c); 
-        let j=saitAdapt[c][0];
-        let k=saitAdapt[c][1];
-        console.log(j);
-        console.log(k);
+        j=calc.saitAdapt[c][0];
+        k=calc.saitAdapt[c][1];
+        if (c == 0 ) {
+            alert("выбери АДАПТИВНОСТЬ");
+        };
+    return;
 }
 
-function f(){
-   
+
+function f(saitId, dizId, adaptId){
+    alert("цена : " + (d+g+j) + "$" + " и займет : " + (e+h+k) + " дней" );
 }
-
-*/
-/*let d = saitTen[a];
-        console.log(d);
-let i = saitDiz[b];
-    console.log(i);
-let e = saitAdapt[c];
-    console.log(e);*/
-
-/*let ten = prompt ("Выбери стиль сайта 1-Сайт визитка 2-Сайт блог 3-многостраничный сайт");
-    console.log(ten);
-let diz = prompt ("Выбери дизайн сайта 1-универсальный 2-с макета psd 3-уникальный");
-    console.log(diz);
-let ad = prompt ("Выбери адаптацию сайта 1-пк 2-мобилый 3-пк и мобильный");
-    console.log(ad);
-function get(i){
-    if (ten <= 3){    
-    let i = ten;
-    alert(i);
-        }
-}*/
-/*alert(ten);
-alert(diz);
-alert(ad);*/
